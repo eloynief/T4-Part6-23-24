3632509076f38236e79a33acbc1e568d42cc27aa
enieves <enieves@106-02.info2>
2024-02-14 10:10:07 +0100

    terminar 8

--- src/ej7/Main.java
+++ src/ej7/Main.java
@@ -10,14 +10,53 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		//
-		String cad1="";
+		String frase="";
 		
 		//
-		String cad2 = "";
+		String palabra = "";
+		
+		//array de cadenas que separa la frase por espacios
+		String[] separacion;
+		
+		int contador=0;
 		
 		
+		//
+		System.out.println("Escribe la frase: ");
+
+		//asignamos valor a la frase
+		frase=sc.nextLine();
 		
+		//asignamos el valor de la separacion
+		separacion=frase.split(" ");
 		
+		//
+		System.out.println("Ahora escribe la palabra: ");
+		
+		//asignamos valor a palabra
+		palabra=sc.nextLine();
+		
+		for(int i=0;i<separacion.length;i++) {
+			
+			if(palabra.equals(separacion[i])) {
+				contador++;
+			}
+			
+		}
+		
+		//comprobar si el numero aparece 1 o mas veces
+		if(contador==1) {
+
+			//
+			System.out.println("La palabra aparece "+contador+" vez");
+			
+		}
+		else {
+
+			//
+			System.out.println("La palabra aparece "+contador+" veces");
+			
+		}
 		
 		
 	}
